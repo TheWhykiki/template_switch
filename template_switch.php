6d1463530cb292ba1f03088f73e65797aaa9fee2
TheWhykiki <info@whykiki.de>
2021-05-26 17:39:45 +0200

    Switcher JS korrekt laden
    JUri::root() einbauen

--- template_switch.php
+++ template_switch.php
@@ -51,7 +51,7 @@ class PlgSystemTemplate_switch extends JPlugin
 
         if ($this->app->isSite())
         {
-            $this->document->addScript('../plugins/system/template_switch/assets/js/switcher.js');
+            $this->document->addScript( JUri::root() . 'plugins/system/template_switch/assets/js/switcher.js');
         }
 
         $stylesheets = $this->document->_styleSheets;
